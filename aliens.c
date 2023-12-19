2b40127081c3b77d59ad42d26f70fb2084c9c3cc
vheczkoslav <vojtech.heczko@gmail.com>
2023-12-19 14:13:03 +0100

    for my colleague

--- aliens.c
+++ aliens.c
@@ -6,43 +6,53 @@ void a_start_pos(Alien* aliens, int count){
         int this_row = (int) i / 11;
         switch (this_row) {
             case 0:
+                int this_y = 70;
                 aliens[i].a_type = TOP_KLINGON;
-                aliens[i].texture_width = 6;
-                aliens[i].texture_height = 7;
+                aliens[i].texture_width = 12;
+                aliens[i].texture_height = 14;
                 aliens[i].score = 25;
                 aliens[i].alive = true;
                 aliens[i].id = i;
+                aliens[i].y = y;
             break;
             case 1:
+                int this_y = 81;
                 aliens[i].a_type = MID_VULCAN;
-                aliens[i].texture_width = 10;
-                aliens[i].texture_height = 7;
+                aliens[i].texture_width = 20;
+                aliens[i].texture_height = 14;
                 aliens[i].score = 20;
                 aliens[i].alive = true;
                 aliens[i].id = i;
+                aliens[i].y = y;
                 break;
             case 2:
+                int this_y = 91;
                 aliens[i].a_type = MID_VULCAN;
-                aliens[i].texture_width = 10;
-                aliens[i].texture_height = 7;
+                aliens[i].texture_width = 20;
+                aliens[i].texture_height = 14;
                 aliens[i].score = 15;
                 aliens[i].alive = true;
                 aliens[i].id = i;
+                aliens[i].y = y;
             case 3:
+                int this_y = 101;
                 aliens[i].a_type = BOT_REPTILIAN;
-                aliens[i].texture_width = 10;
-                aliens[i].texture_height = 7;
+                aliens[i].texture_width = 20;
+                aliens[i].texture_height = 14;
                 aliens[i].score = 10;
                 aliens[i].alive = true;
                 aliens[i].id = i;
+                aliens[i].y = y;
                 break;
             case 4:
+                int this_y = 111;
                 aliens[i].a_type = BOT_REPTILIAN;
-                aliens[i].texture_width = 10;
-                aliens[i].texture_height = 7;
+                aliens[i].texture_width = 20;
+                aliens[i].texture_height = 14;
                 aliens[i].score = 5;
                 aliens[i].alive = true;
                 aliens[i].id = i;
+                aliens[i].y = y;
                 break;
             default:
                 perror("This... This should NOT happen :O\n");
@@ -86,9 +96,12 @@ void add_a_textures(Alien* aliens, SDL_Renderer* renderer, int count){
                 exit(0);
         }
     }
+    return;
 }
 
-// void kill_alien(Alien* aliens,
+void kill_alien(Alien* aliens, int index){
+    return;
+}
 
 void render_aliens(Alien* aliens, int tick){
     // Render is from top to bottom, from index 0 to index 54
@@ -103,4 +116,5 @@ void free_aliens(Alien* aliens, int count){
         SDL_DestroyTexture(aliens[i].texture[1]); aliens[i].texture = NULL;
     }
     free(aliens);
+    return;
 }
--- aliens.h
+++ aliens.h
@@ -5,10 +5,8 @@
 typedef enum {TOP_KLINGON, MID_VULCAN, BOT_REPTILIAN, UFO_SPECIAL}alien_type;
 // TOP = 6x7, MID & BOT = 10x7, SPECIAL = 14x6
 typedef struct{
-    int x;
-    int y;
+    SDL_Rect rect;
     bool alive;
-    SDL_Texture** texture;
     int texture_width;
     int texture_height;
     alien_type a_type;
@@ -18,5 +16,6 @@ typedef struct{
 
 void a_start_pos(Alien* aliens, int count);
 void add_a_textures(Alien* aliens, SDL_Renderer* renderer, int count);
+//void kill_alien(Alien* aliens, int index);
 void render_aliens(Alien* aliens, int tick);
 void free_aliens(Alien* aliens, int count);
